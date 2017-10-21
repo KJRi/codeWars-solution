33ca5cbfae6e25584278ca3741109a09855c6e5c
KJR <kjrkjri@outlook.com>
2017-10-21 23:10:50 +0800

    2

--- 10-19.js
+++ 10-19.js
@@ -1,5 +1,19 @@
 /*\n
  * 
- * topic:
+ * topic: kyu
+Give me the k first digits. Odd digits.
+
+Instruction
+You're given two integers – n and k. n is always positive, while k >= 0. Write a function that returns first k odd digits in a number n.
+
+The output should also be an integer.
+
+In the following cases a function should return 0:
+
+there are no odd digits in a number n;
+k is bigger than a number of digits in n;
+k = 0;
+k is bigger than a number of odd digits in n.
+BTW, is 0 an odd number or an even one?
  * 
  */
